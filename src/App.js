d8a8b88aed91e6b70d769ecf546201339baa5985
Gabriel Correia <101719012+Gabriel06112001@users.noreply.github.com>
2022-04-29 10:05:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './fundo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    
+      <header className="App-header"> header
+
+         <div className="cabecalho"> 
+          </div>
+          <div className="menu">
+
+          </div>
+
+           <img src={logo} className="fundo" alt="" />
+       
+      
+      </header>
+    
+  );
+}
+
+export default App;
